051d751d088aefc75f475126e966a10f23c212a7
Andrew Koebbe <abk@stowers.org>
2016-04-06 19:44:34 -0500

    Better Version

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-$(window).scroll(function(){
-    var scrollPos = $(window).scrollTop();
+$(document).ready(function() {
     var contentHeight = $('#main-content').height();
 
-    var scrollPercent = scrollPos/contentHeight;
+    $('nav a').each(function() {
+        var sectionId = $(this).attr('href');
+        var sectionHeight = $(sectionId).height();
+        var sectionPercentHeight = sectionHeight/contentHeight;
 
-    console.log('Windows Scroll %:'+(scrollPercent * 100));
+        $(this).css('margin-bottom', sectionPercentHeight * $(window).height() * .5);
 
-    var navHeight = $('nav').height();
-    var navOffset = 0 - navHeight * scrollPercent;
+    });
 
-    console.log('Nav Scroll Offset:' + navOffset);
-    $('nav').css({top: navOffset});
+    $('nav a').last().css('margin-bottom', '');
 
 
+    $(window).scroll(function(){
+        var scrollPos = $(window).scrollTop();
 
+        var scrollPercent = scrollPos/contentHeight;
+
+        console.log('Windows Scroll %:'+(scrollPercent * 100));
+
+        var navHeight = $('nav').height();
+        var navOffset = 0 - navHeight * scrollPercent;
+
+        console.log('Nav Scroll Offset:' + navOffset);
+        $('nav').css({top: navOffset});
+
+    });
 });
+
+
+
